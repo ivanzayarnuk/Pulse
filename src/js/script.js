32ac2b28e3097bc0ecfe6d77f990ca2b7c36e2dd
Ivan zayarnuk <zayarnukivan@gmail.com>
2021-11-08 17:12:11 +0200

    add slider SLICK

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        // dots: true,
+        // infinite: true,
+        speed: 1100,            // speed 1500 mseconds
+        // adaptiveHeight: true,
+        // autoplay: true,
+        // autoplaySpeed: 1100,
+        // fade: true,
+        // cssEase: 'Linear'
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/right.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: false,
+                arrows: false
+              }
+            }
+          ]
+      });
+  });
