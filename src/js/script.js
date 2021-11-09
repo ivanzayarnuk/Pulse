054dc7a777cca6570a644d1cd55a3c82176e8ea0
Ivan zayarnuk <zayarnukivan@gmail.com>
2021-11-09 20:16:20 +0200

    add tabs and JavaScript

--- src/js/script.js
+++ src/js/script.js
@@ -18,11 +18,36 @@ $(document).ready(function(){
               breakpoint: 992,
               settings: {
                 dots: false,
-                arrows: false
+                arrows: false,
+                mobileFirst:true,
+                autoplay: true,
+                autoplaySpeed: 1100
               }
             }
           ]
       });
-  });
+
+      /* Переключення табів*/
+        $('.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function(){
+            $(this).addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+                .closest('div.container').find('div.catalog__content')
+                .removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
+
+        function toggleSlide(item){
+                
+            $(item).each(function(i){
+                $(this).on('click', function (e){
+                    e.preventDefault();
+                    $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                    $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+                });
+            });
+        }
+        /* Натиснення на ссилку подробно і назад */
+        toggleSlide('.catalog-item__link');
+        toggleSlide('.catalog-item__back');
+
+});
 
   /* End carousel */
